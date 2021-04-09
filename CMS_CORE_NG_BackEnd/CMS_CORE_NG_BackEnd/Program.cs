e3bc7d20270abdcc2ffb7aca7f796f6d6f6a3ae0
HeshanHH <heshanwijerathna01@gmail.com>
2021-04-09 15:16:34 +0530

    Create LoggingService and install SeriLog. add logging to build method also

--- CMS_CORE_NG_BackEnd/CMS_CORE_NG_BackEnd/Program.cs
+++ CMS_CORE_NG_BackEnd/CMS_CORE_NG_BackEnd/Program.cs
@@ -1,9 +1,13 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,13 +17,37 @@ namespace CMS_CORE_NG_BackEnd
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            // logging to catche errors even app is  building.
+            // by creating scope we can consiume the UseSerilog define on CreateHostBuilder.
+            using (var scope = host.Services.CreateScope())
+            {
+
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseSerilog((hostingContext, loggingConfiguration) => loggingConfiguration
+                    .Enrich.FromLogContext()
+                    // printing usefull informations. (ex: if we want to kill a sertaing task  we can use ProcessId)
+                      .Enrich.WithProperty("Application", "CMS_CORE_NG")
+                      .Enrich.WithProperty("MachineName", Environment.MachineName)
+                      .Enrich.WithProperty("CurrentManagedThreadId", Environment.CurrentManagedThreadId)
+                      .Enrich.WithProperty("OSVersion", Environment.OSVersion)
+                      .Enrich.WithProperty("Version", Environment.Version)
+                      .Enrich.WithProperty("UserName", Environment.UserName)
+                      .Enrich.WithProperty("ProcessId", Process.GetCurrentProcess().Id)
+                      .Enrich.WithProperty("ProcessName", Process.GetCurrentProcess().ProcessName)
+                      //.WriteTo.Console(theme: CustomConsoleTheme.VisualStudioMacLight)
+                      .WriteTo.File(//formatter: new CustomTextFormatter(), 
+                      path: Path.Combine(hostingContext.HostingEnvironment.ContentRootPath + $"{Path.DirectorySeparatorChar}Logs{Path.DirectorySeparatorChar}", $"cms_core_ng_{DateTime.Now:yyyyMMdd}.txt"))
+                     .ReadFrom.Configuration(hostingContext.Configuration));
                     webBuilder.UseStartup<Startup>();
                 });
     }
